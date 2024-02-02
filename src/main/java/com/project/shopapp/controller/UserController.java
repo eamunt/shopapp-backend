ebeaeaa2998a91e9bb14f6c78974f5b5b36d2a26
HaoNguyen <itshaonguyen@gmail.com>
2024-02-02 17:14:12 +0700

    feat(users): UserController / DTOs / register / login

---
+++ src/main/java/com/project/shopapp/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.project.shopapp.controller;
+
+
+import com.project.shopapp.dtos.UserDTO;
+import com.project.shopapp.dtos.UserLoginDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+    @PostMapping("/register")
+    public ResponseEntity<?> createUser(
+            @Valid @RequestBody UserDTO userDTO,
+            BindingResult result) {
+        try {
+            if(result.hasErrors()){
+                List<String> errorMessages = result.getFieldErrors()
+                        .stream()
+                        .map(fieldError -> fieldError.getDefaultMessage())
+                        .toList();
+                return ResponseEntity.badRequest().body(errorMessages);
+            }
+            if(!userDTO.getPassword().equals(userDTO.getRetypePassword())){
+                return ResponseEntity.badRequest().body("Retype password does not match");
+            }
+            return ResponseEntity.ok("Register successfully");
+        }catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@Valid @RequestBody UserLoginDTO userLoginDTO) {
+        // Kiểm tra thông tin đăng nhập và sinh ra token
+
+        // Return token in response
+        return ResponseEntity.ok("some token");
+    }
+}
