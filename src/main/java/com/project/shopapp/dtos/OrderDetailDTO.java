cab50c34aa89214a79e51f5ac77b9d32bcb581a5
HaoNguyen <itshaonguyen@gmail.com>
2024-02-16 16:47:09 +0700

    feat(models): add OrderDetails (DTO + controller) / config application.yml/datasource,jpa / add Models (BaseEntity, Category, Product)

---
+++ src/main/java/com/project/shopapp/dtos/OrderDetailDTO.java
@@ -0,0 +1,34 @@
+package com.project.shopapp.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDetailDTO {
+    @JsonProperty("order_id")
+    @Min(value=1, message="Order's ID must be > 0")
+    private Long orderId;
+
+    @JsonProperty("product_id")
+    @Min(value=1, message="Product's ID must be > 0")
+    private Long productId;
+
+    @Min(value=0, message="Order's ID must be > 0")
+    private Long price;
+
+    @Min(value=1, message="Order's ID must be > 0")
+    @JsonProperty("number_of_products")
+    private int numberOfProducts;
+
+    @Min(value=1, message="Order's ID must be > 0")
+    @JsonProperty("total_money")
+    private float totalMoney;
+
+    private String color;
+
+}
