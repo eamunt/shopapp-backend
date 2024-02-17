f920c97e35cff32d9db5dbae045ca9858af4d497
HaoNguyen <itshaonguyen@gmail.com>
2024-02-17 15:19:05 +0700

    feat(models): Order (OrderStatus) / OrderDetail

---
+++ src/main/java/com/project/shopapp/models/Order.java
@@ -0,0 +1,74 @@
+package com.project.shopapp.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User userId;
+
+    @Column(name = "fullname", length = 100)
+    private String fullName;
+
+    @Column(name = "email", length = 100)
+    private String email;
+
+    @Column(name = "phone_number", length = 10, nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "address", length = 200)
+    private String address;
+
+    @Column(name = "note", length = 100)
+    private String note;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "total_money")
+    private Float totalMoney;
+
+    @Column(name = "shipping_method", length = 100)
+    private String shippingMethod;
+
+    @Column(name = "shipping_address", length = 200)
+    private String shippingAddress;
+
+    @Column(name = "shipping_date")
+    private Date shippingDate;
+
+    @Column(name = "tracking_number", length = 100)
+    private String trackingNumber;
+
+    @Column(name = "payment_method", length = 100)
+    private String paymentMethod;
+
+    @Column(name = "active")
+    private boolean active;
+
+
+
+
+
+
+}
