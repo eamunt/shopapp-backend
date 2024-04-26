8cba1b2d5de762fffbf046f8e909379269a5b3a9
haostudio <itshaonguyen@gmail.com>
2024-04-26 14:15:24 +0700

    refactor(): fix CORS

--- src/main/java/com/project/shopapp/configuations/WebSecurityConfig.java
+++ src/main/java/com/project/shopapp/configuations/WebSecurityConfig.java
@@ -87,21 +87,9 @@ public class WebSecurityConfig {
 
                             .anyRequest()
                             .authenticated();
-                });
-        httpSecurity.cors(new Customizer<CorsConfigurer<HttpSecurity>>() {
-            @Override
-            public void customize(CorsConfigurer<HttpSecurity> httpSecurityCorsConfigurer) {
-                CorsConfiguration configuration = new CorsConfiguration();
-                configuration.setAllowedOrigins(List.of("*"));
-                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT",
-                        "PATCH", "DELETE", "OPTIONS"));
-                configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-                configuration.setExposedHeaders(List.of("x-auth-token"));
-                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-                source.registerCorsConfiguration("/**", configuration);
-                httpSecurityCorsConfigurer.configurationSource(source);
-            }
-        });
+                })
+                .csrf(AbstractHttpConfigurer::disable);
+
         httpSecurity.securityMatcher(String.valueOf(EndpointRequest.toAnyEndpoint()));
         return httpSecurity.build();
     }
